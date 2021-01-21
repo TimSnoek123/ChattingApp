32c41ec94c582c5410e1be3bc3995121d02bf15e
timsn <timsn@DESKTOP-QFCRRQ2>
2021-01-21 17:40:03 +0100

    Init commit

---
+++ ChattingApp/Hubs/WebRtcHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChattingApp.Hubs
+{
+    public class WebRtcHub : Hub
+    {
+        public async Task OnNewUserAsync(string username)
+        {
+            await Clients.Others.SendAsync("OnNewUser", username, Context.ConnectionId);
+        }
+
+        public async Task OnOfferAsync(string id, string localDescription)
+        {
+            await Clients.Others.SendAsync("OnOffer", id, localDescription);
+        }
+
+        public async Task OnCandidateAsync(string id, string candidate)
+        {
+            await Clients.Others.SendAsync("OnCandidate", id, candidate);
+        }
+
+        public async Task OnAnswerAsync(string id, string localDescription)
+        {
+            await Clients.Others.SendAsync("OnAnswer", id, localDescription);
+        }
+    }
+}
---
+++ ChattingApp/wwwroot/js/WebRtc.js
@@ -0,0 +1,116 @@
+﻿const peerConnections = {};
+const config = {
+    "iceServers":
+        [
+            { "urls": "stun:74.125.142.127:19302" },
+            { "urls": "turn:turn.anyfirewall.com:443?transport=tcp", "username": "webrtc", "credential": "webrtc" }
+        ]
+};
+
+var audio = new Audio();
+var localStream;
+
+navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start();
+    
+    localStream = stream;
+    audio.onloadedmetadata = function (ev) {
+
+        // Play the audio in the 2nd audio 
+        // element what is being recorded 
+        console.log("PLAYT");
+        audio.play();
+    }; 
+})
+var dotnetHelper = null;
+
+function SetDotnetHelper(helper){
+    dotnetHelper = helper;
+}
+
+function CreateRTCConnection(id) {
+    const peerConnection = new RTCPeerConnection(config);
+
+    peerConnection.oniceconnectionstatechange = event => {
+        console.log("connection changed");
+        console.log(peerConnection);
+    }
+
+    peerConnections[id] = peerConnection;
+
+    let stream = localStream;
+
+    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+    peerConnection.onicecandidate = event => {
+        if (event.candidate) {
+            console.log("Sending candidate");
+            dotnetHelper.invokeMethodAsync("SendCandidateAsync", id, JSON.stringify(event.candidate));
+        }
+    }
+
+    peerConnection.ontrack = event => {
+        console.log("GOT TRACK");
+        console.log(event.streams);
+        audio.srcObject = event.streams[0];
+        console.log(peerConnection.connectionState);
+    };
+
+    peerConnection
+        .createOffer()
+        .then(sdp => peerConnection.setLocalDescription(sdp))
+        .then(() => {
+            console.log("Sending offer");
+            dotnetHelper.invokeMethodAsync("SendOfferAsync", id, JSON.stringify(peerConnection.localDescription));
+        });
+
+}
+
+function addCandidate(id, candidate) {
+    console.log("ADDING CANDIDATE");
+    peerConnections[id].addIceCandidate(new RTCIceCandidate(JSON.parse(candidate)));
+}
+
+function setRemoteDescription(id, description) {
+    console.log("GOT ANSWER");
+    peerConnections[id].setRemoteDescription(JSON.parse(description));
+}
+
+function onOffer(id, description) {
+    const peerConnection = new RTCPeerConnection(config);
+
+    peerConnection.oniceconnectionstatechange = event => {
+        console.log("connection changed");
+        console.log(peerConnection);
+    }
+
+    let stream = localStream;
+
+    stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+    peerConnection
+        .setRemoteDescription(JSON.parse(description))
+        .then(() => peerConnection.createAnswer())
+        .then(sdp => peerConnection.setLocalDescription(sdp))
+        .then(() => {
+            console.log("Sending answer");
+            dotnetHelper.invokeMethodAsync("SendAnswerAsync", id, JSON.stringify(peerConnection.localDescription));
+        });
+
+
+    peerConnection.ontrack = event => {
+        console.log("GOT TRACK");
+        console.log(event.streams);
+        audio.srcObject = event.streams[0];
+        console.log(peerConnection.connectionState);
+    };
+
+    peerConnection.onicecandidate = event => {
+        if (event.candidate) {
+            console.log("Sending candidate");
+            dotnetHelper.invokeMethodAsync("SendCandidateAsync", id, JSON.stringify(event.candidate));
+        }
+    }
+
+}
